7a45eeefe91b3ee5fac14cde89a1eebca8f2ca3a
Justin Mekjean <jmekjean@campuslabs.com>
2020-02-11 14:03:11 -0500

    this commit

---
+++ WishList/Models/Item.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace WishList.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Description { get; set; }
+
+    }
+}
